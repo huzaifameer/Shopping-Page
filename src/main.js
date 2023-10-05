4b9a6f13fecc0cb7b14579344e8d871f0ccd5280
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-10-05 23:58:22 +0530

    commit14

--- src/main.js
+++ src/main.js
@@ -20,3 +20,10 @@ for (let i=0;i<triggerOpen.length;i++){
 
 /*---mobile menu sub-menu---*/
 const submenu=document.querySelectorAll('.child-trigger');
+submenu.forEach((menu)=> menu.addEventListener('click',function (e){
+    e.preventDefault();
+    submenu.forEach((item) => item !== this ? item.closest('.has-child').classList.remove('active'):null);
+    if (this.closest('.has-child').classList !== 'active'){
+        this.closest('.has-child').classList.toggle('active');
+    }
+}))
