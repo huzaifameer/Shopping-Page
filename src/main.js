2be05499b803facc2268816b4e644b4d75e9061b
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-10-07 01:38:38 +0530

    commit18

--- src/main.js
+++ src/main.js
@@ -51,4 +51,21 @@ const carousel = new Swiper('.carouselbox', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints:{
+        481:{
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            centeredSlides: false
+        },
+        640:{
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            centeredSlides: false
+        },
+        992:{
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            centeredSlides: false
+        }
+    }
 });
