c7c648a4e0b15d5074e946b5fa2bb78b17fe8d96
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-10-07 20:26:01 +0530

    commitBlogs

--- src/main.js
+++ src/main.js
@@ -43,6 +43,20 @@ if (sorter){
         sorter.querySelector('ul').classList.toggle('show')
     }))
 }
+//tabbed
+const trigger=document.querySelectorAll('.tabbed-trigger'),
+    content=document.querySelectorAll('.tabbed > div');
+trigger.forEach((btn)=>{
+    btn.addEventListener('click',function (){
+        let dataTarget=this.dataset.id,
+            body=document.querySelector(`#${dataTarget}`);
+
+        trigger.forEach((b) => b.parentNode.classList.remove('active'));
+        trigger.forEach((s) => s.classList.remove('active'));
+        this.parentNode.classList.add('active');
+        body.classList.add('active');
+    })
+})
 
 //slider
 const swiper = new Swiper('.sliderbox', {
